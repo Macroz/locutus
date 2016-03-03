97c85d6bda94d639674fbe00399c7577178c9b18
Markku Rontu <markku.rontu@iki.fi>
2016-03-03 20:32:58 +0200

    feat: WebSocket echo handler

--- src/locutus/api.clj
+++ src/locutus/api.clj
@@ -1,10 +1,18 @@
 (ns locutus.api
-  (:require [bidi.vhosts :as bidi]
+  (:require [aleph.http :as http]
+            [manifold.stream :as s]
+            [bidi.vhosts :as bidi]
             [yada.yada :as yada :refer [yada]]
             [locutus.beacon :as beacon]))
 
+(defn websocket-handler [req]
+  (let [s @(http/websocket-connection req)]
+    (println "Got ws!")
+    (s/connect s s)))
+
 (defn api []
-  ["" [["/" (yada "Resistance is futile!")]
+  ["" [["/ws" websocket-handler]
+       ["/" (yada "Resistance is futile!")]
        ["/beacons" (yada beacon/known-beacons)]
        ["/observations" (yada {:msgs ["We are the borg."]})]
        [true (fn [req] {:status 404 :body "404"})]]])
