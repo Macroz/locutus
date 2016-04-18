b9c33c391ebdda81e949cc86652f3a5ed361d688
Markku Rontu <markku.rontu@iki.fi>
2016-04-18 22:35:24 +0300

    chore: bump dependencies

--- project.clj
+++ project.clj
@@ -3,14 +3,17 @@
   :url "http://github.com/Macroz/locutus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [aleph "0.4.1-beta4" :exclusions [org.clojure/clojure byte-streams]]
                  [aero "0.1.4" :exclusions [prismatic/schema]]
                  [bidi "2.0.0"]
                  [yada "1.1.0-20160225.101107-28"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/core.async "0.2.374"]
-                 [mount "0.1.9"]]
+                 [mount "0.1.9"]
+                 [io.moquette/moquette-broker "0.8.1"]
+                 [clojurewerkz/propertied "1.2.0"]]
+  :repositories [["jcenter" "http://jcenter.bintray.com/"]]
   :repl-options {:init-ns user
                  :welcome (println "Resistance is futile!")}
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.7.0"]
