56cac9905a7ac557e6def07481d4db4d6a20bbff
Markku Rontu <markku.rontu@iki.fi>
2016-05-10 20:05:14 +0300

    clean: api

--- src/locutus/api.clj
+++ src/locutus/api.clj
@@ -4,39 +4,10 @@
             [manifold.stream :as s]
             [bidi.vhosts :as bidi]
             [yada.yada :as yada :refer [yada]]
-            [locutus.beacon :as beacon]
-            [locutus.broker :as broker]))
-
-(defn read-mqtt-fixed-header [[h l]]
-  {:type (bit-shift-right (bit-and 240 h) 4)
-   :flags (bit-and 15 h)
-   :length l})
-
-(defn to-bin [b]
-  (.replaceAll (format "%8s" (Integer/toBinaryString b)) " " "0"))
-
-;;(def adapter (broker/make-netty (async/chan)))
-
-(defn websocket-handler [req]
-  (println req)
-  ;;(adapter req)
-  #_(let [s @(http/websocket-connection req)
-        in-ch (async/chan)
-        out-ch (async/chan)]
-    (println "Connected ws!")
-    (s/connect s in-ch)
-    (s/connect out-ch s)
-    (async/go-loop []
-      (let [x (async/<! in-ch)]
-        (when x
-          (println "data" (map to-bin x))
-          (println "head" (read-mqtt-fixed-header (take 2 x)))
-          (async/>! out-ch (str "hello " x))
-          (recur))))))
+            [locutus.beacon :as beacon]))
 
 (defn api []
-  ["" [;;["/ws" websocket-handler]
-       ["/" (yada "Resistance is futile!")]
+  ["" [["/" (yada "Resistance is futile!")]
        ["/beacons" (yada beacon/known-beacons)]
        ["/observations" (yada {:msgs ["We are the borg."]})]
        [true (fn [req] {:status 404 :body "404"})]]])
